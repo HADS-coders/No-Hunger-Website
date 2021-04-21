cdbb4a59abceab8bea28aea53bda8e6483a95037
Daniyal Dolare <daniyal.dolare@gmail.com>
2021-04-21 12:39:33 +0530

    try catch added for db

--- includes/config.php
+++ includes/config.php
@@ -6,12 +6,17 @@
     $db_name = 'd4eg82tgso2ql5';
     $db_port = '5432';
 
+    try{
     $db = new PDO('pgsql:host='.$db_host.';port='.$db_port.';dbname='.$db_name.';charset=utf8',$db_user,$db_password);
 
     // set db attributes
     $db->setAttribute(PDO::ATTR_EMULATE_PREPARES,false);
-    $db->setAttribute(PDO::MYSQL_ATTR_USE_BUFFERED_QUERY,true);
+    $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE,PDO::FETCH_OBJ);
+    // $db->setAttribute(PDO::MYSQL_ATTR_USE_BUFFERED_QUERY,true);
     $db->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
-
     
+    }
+    catch (PDOException $e){
+        echo 'Connection failed: '.$e->getMessage();
+    }
 ?>
