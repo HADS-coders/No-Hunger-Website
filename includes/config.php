edd2852bf8f3db7b0c8dfa03fd8dadaa91c4dd12
Daniyal Dolare <daniyal.dolare@gmail.com>
2021-04-21 18:28:20 +0530

    updated

--- includes/config.php
+++ includes/config.php
@@ -16,6 +16,17 @@
     $db->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
     echo 'Connected';
     
+    $query = 'SELECT * from person';
+        
+            //prepare statement
+            $stmt = $this->conn->prepare($query);
+
+            //execute 
+            $stmt->execute();
+
+            $details = $stmt->fetch();
+
+            print_r($details);
     }
     catch (PDOException $e){
         echo 'Connection failed: '.$e->getMessage();
