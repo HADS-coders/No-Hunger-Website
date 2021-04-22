4eedd3574cd947085037ced37f948270769dcbb6
Daniyal Dolare <daniyal.dolare@gmail.com>
2021-04-22 12:49:00 +0530

    updated

--- api/read.php
+++ api/read.php
@@ -25,13 +25,13 @@ try{
         while($row = $result->fetch(PDO::FETCH_ASSOC)){
             extract($row);
             $person_item = array(
-                'pid' => $PID,
-                'fname' => $fname,
-                'lname' => $lname,
-                'email' => $email,
-                'phone' => $phone,
-                'username' => $username,
-                'password' => $password,
+                'pid' => $row[0],
+                'fname' => $row[1],
+                'lname' => $row[2],
+                'email' => $row[3],
+                'phone' => $row[4],
+                'username' => $row[5],
+                'password' => $row[6],
             );
             array_push($person_arr['data'], $person_item);
         }
