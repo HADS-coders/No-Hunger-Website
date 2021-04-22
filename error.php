3f16180eed49fa7ef989af70d5f9882955b753de
Daniyal Dolare <daniyal.dolare@gmail.com>
2021-04-22 18:15:58 +0530

    website added for test

---
+++ error.php
@@ -0,0 +1,34 @@
+<html>
+
+<body >
+<head>
+<style>
+ .text{
+  width: 447px;
+  height: 300px;
+    float: left;
+    padding-left: 30px;
+    border:3px solid rgb(0, 0, 0);
+    border-radius: 8px;
+  
+   margin: 106px 80px;
+   margin-left: 500px;
+  margin-top: 50px;
+ box-shadow: 0 14px 28px rgba(255, 0, 0, 0.25), 
+			0 10px 10px rgba(255, 230, 0, 0.767);
+ }
+</style>
+</head>
+<div class="text">
+     <h1>
+       <p style="text-align: center;">
+         <b style="color: rgb(255, 4, 79);">
+         LOGIN FAILED!!!<br><br><br>
+      
+          Please enter correct email and password
+         </b>
+       </p>
+     </h1>
+   </div>
+   </body>
+   </html>
