4109e5b21d6643da0cfed65e37c5eb947bcd1fea
Daniyal Dolare <daniyal.dolare@gmail.com>
2021-04-22 20:16:00 +0530

    mysql db added

--- connection.php
+++ connection.php
@@ -1,8 +1,8 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "hunger";
+$servername = "remotemysql.com";
+$username = "qW0jycIN5x";
+$password = "Q3xqkQtsDA";
+$dbname = "qW0jycIN5x";
 
 $conn = mysqli_connect($servername,$username,$password,$dbname);
 if($conn)
