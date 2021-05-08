e7f7a6139c2c6451ea365b756cbf167a2d0e15af
Daniyal Dolare <daniyal.dolare@gmail.com>
2021-05-08 19:54:54 +0530

    assoc added

--- api/getDonationRequests.php
+++ api/getDonationRequests.php
@@ -9,7 +9,7 @@ include_once('../connection.php');
 $query = 'SELECT longitude,latitude from donation';
 
 $result = mysqli_query($conn,$query);
-$data = $result->fetch_all();
+$data = $result->fetch_all(MYSQLI_ASSOC);
 foreach ($data as $d) {
     echo $d['longitude'];
 }
