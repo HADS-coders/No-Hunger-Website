e64c789940edee832751293128f7139f51b6a1d2
Daniyal Dolare <daniyal.dolare@gmail.com>
2021-05-08 19:51:03 +0530

    getDonationRequests test

---
+++ api/getDonationRequests.php
@@ -0,0 +1,17 @@
+<?php
+
+//headers
+header('Access-Control-Allow-Origin: *');
+header('Content-Type: application/json');
+
+include_once('../connection.php');
+
+$query = 'SELECT longitude,latitude from donation';
+
+$result = mysqli_query($conn,$query);
+$data = $result->fetch_all();
+foreach ($data as $d) {
+    echo $d->longitude;
+}
+
+?>
