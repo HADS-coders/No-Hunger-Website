30e24e9edf9df83dc6504c6428931e787ec28def
Daniyal Dolare <daniyal.dolare@gmail.com>
2021-05-09 00:17:38 +0530

    testing get request

--- api/getDonationRequests.php
+++ api/getDonationRequests.php
@@ -6,22 +6,117 @@ header('Content-Type: application/json');
 
 include_once('../connection.php');
 
+//Get volunterr lat lang from get req
 
+$vol_latitude = isset($_GET['latitude']) ? $_GET['latitude'] : die();
+$vol_longitude = isset($_GET['longitude']) ? $_GET['longitude'] : die();
+$range = isset($_GET['range']) ? $_GET['range'] : die();
 
-$query = 'SELECT longitude,latitude from donation ';
+//get all donation req lat lab from db
 
-$donation_id = isset($_GET['donation_id']) ? $_GET['donation_id'] : die();
-echo $donation_id;
+$query = 'SELECT donation_id,longitude,latitude from donation ';
+
 $stmt = $conn->prepare($query);
-// $stmt->bind_param(1,$donation_id);\
+
 $stmt->execute();
+
 /* bind result variables */
-$stmt->bind_result($longitude, $latitude);
+$stmt->bind_result($donation_id,$longitude, $latitude);
 
+$filtered_id = array();
 
-while( $stmt->fetch()){
-    echo $longitude;
+while($stmt->fetch()){
+
+    $distance = getDistanceBetweenPointsNew($latitude,$longitude,$vol_latitude,$vol_longitude,'kilometers');
+    if($distance <= $range){
+        array_push($filtered_id,$donation_id);
+    }
 }
+
+echo json_encode($filtered_id);
+
+$filtered_data = array();
+$filtered_data['data'] = array();
+
+//for each id in filtered_id , get and save data to list
+foreach($filtered_id as $id){
+
+    $request = array();
+    $donation = array();
+    $food=array();
+    $foodItems = array();
+
+    //get donation detail 
+    $query = 'select * from donation where donation_id = '.$id;
+    $result = $conn->query($query);
+    $rows = $result->fetch_all(MYSQLI_ASSOC);
+    foreach($rows as $row){
+        $donation = array(
+            'donation_id'=>$row['donation_id'],
+            'name'=>$row['name'],
+            'email'=>$row['email'],
+            'number'=>$row['number'],
+            'time'=>$row['time'],
+            'latitude'=>$row['latitude'],
+            'longitude'=>$row['longitude'],
+        );
+        $donation_id = $row['donation_id'];
+
+        //get food detail
+        $query = 'select * from food where donation_id = '.$id;
+        $result = $conn->query($query);
+        $rows = $result->fetch_all(MYSQLI_ASSOC);
+        $food_id = 0;
+        foreach($rows as $row){
+            $food = array(
+                'food_id'=>$row['food_id'],
+                'type'=>$row['type'],
+                'havePackets'=>$row['havePackets'],
+                'time'=>$row['time'],
+            );
+            $food_id = $row['food_id'];
+        }
+
+        //get foodItems detail
+        $query = 'select * from foodItem where food_id = '.$food_id;
+        $result = $conn->query($query);
+        $rows = $result->fetch_all(MYSQLI_ASSOC);
+
+        foreach($rows as $row){
+            $foodItem = array(
+                'food_id'=>$row['food_id'],
+                'type'=>$row['type'],
+                'havePackets'=>$row['havePackets'],
+                'time'=>$row['time'],
+            );
+            array_push($foodItems,$foodItem);
+        }
+
+    }
+    $request['donation'] = $donation;
+    $request['food'] = $food;
+    $request['foodItems'] = $foodItems;
+    array_push($filtered_data['data'],$request);
+}
+
+
+
+function getDistanceBetweenPointsNew($latitude1, $longitude1, $latitude2, $longitude2, $unit = 'miles') {
+    $theta = $longitude1 - $longitude2; 
+    $distance = (sin(deg2rad($latitude1)) * sin(deg2rad($latitude2))) + (cos(deg2rad($latitude1)) * cos(deg2rad($latitude2)) * cos(deg2rad($theta))); 
+    $distance = acos($distance); 
+    $distance = rad2deg($distance); 
+    $distance = $distance * 60 * 1.1515; 
+    switch($unit) { 
+      case 'miles': 
+        break; 
+      case 'kilometers' : 
+        $distance = $distance * 1.609344; 
+    } 
+    return (round($distance,2)); 
+  }
+
+
 // while ($row){
 //     echo $longitude.'  '.$latitude;
 // }
