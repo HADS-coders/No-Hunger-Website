be30e87f7d92aecd1078fbaf91f1aa98b241c995
Daniyal Dolare <daniyal.dolare@gmail.com>
2021-05-08 20:06:44 +0530

    test

--- api/getDonationRequests.php
+++ api/getDonationRequests.php
@@ -6,12 +6,17 @@ header('Content-Type: application/json');
 
 include_once('../connection.php');
 
-$query = 'SELECT longitude,latitude from donation';
+
+
+$query = 'SELECT longitude,latitude from donation where donation_id=?';
+
+$stmt = $conn->prepare($query);
+$stmt->bind_param(1,1);
 
 $result = mysqli_query($conn,$query);
-$data = $result->fetch_all(MYSQLI_ASSOC);
-foreach ($data as $d) {
-    echo $d['longitude'];
+$rows = $result->fetch_all(MYSQLI_ASSOC);
+foreach ($rows as $row) {
+    echo $row['longitude'];
 }
 
 ?>
