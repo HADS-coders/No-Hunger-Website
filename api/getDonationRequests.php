c1d54bbcab5a1186f843ce045f2ea1fc788f1a77
Daniyal Dolare <daniyal.dolare@gmail.com>
2021-05-08 20:22:18 +0530

    test

--- api/getDonationRequests.php
+++ api/getDonationRequests.php
@@ -8,14 +8,21 @@ include_once('../connection.php');
 
 
 
-$query = 'SELECT longitude,latitude from donation where donation_id = ?';
+$query = 'SELECT longitude,latitude from donation where donation_id = 1';
 
 $donation_id = isset($_GET['donation_id']) ? $_GET['donation_id'] : die();
+echo $donation_id;
 $stmt = $conn->prepare($query);
-$stmt->bind_param(1,$donation_id);
-
+// $stmt->bind_param(1,$donation_id);\
 $stmt->execute();
-$row = $stmt->fetch(PDO::FETCH_ASSOC);
+/* bind result variables */
+$stmt->bind_result($longitude, $latitude);
+
+
+$row = $stmt->fetch();
+while ($row){
+    echo $longitude.'  '.$latitude;
+}
 echo $row['longitude'];
 // $result = mysqli_query($conn,$query);
 // $rows = $result->fetch_all(MYSQLI_ASSOC);
