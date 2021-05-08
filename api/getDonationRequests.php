19f8079b4a342deb84e30ea4201db065fd60311e
Daniyal Dolare <daniyal.dolare@gmail.com>
2021-05-08 20:12:57 +0530

    test

--- api/getDonationRequests.php
+++ api/getDonationRequests.php
@@ -14,10 +14,13 @@ $donation_id = isset($_GET['donation_id']) ? $_GET['donation_id'] : die();
 $stmt = $conn->prepare($query);
 $stmt->bind_param(1,$donation_id);
 
-$result = mysqli_query($conn,$query);
-$rows = $result->fetch_all(MYSQLI_ASSOC);
-foreach ($rows as $row) {
-    echo $row['longitude'];
-}
+$stmt->execute();
+$row = $stmt->fetch();
+echo $row['longitude'];
+// $result = mysqli_query($conn,$query);
+// $rows = $result->fetch_all(MYSQLI_ASSOC);
+// foreach ($rows as $row) {
+//     echo $row['longitude'];
+// }
 
 ?>
